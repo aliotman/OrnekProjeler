187ac4efd41818caf007e18d9ab2f1d667135f9a
Ali Otman <aliotmangitar@hotmail.com>
2022-08-12 04:01:55 +0300

    Veri Tipleri ve Degiskenler Ornekleri Eklendi

---
+++ veritiplerivedegiskenler/Program.cs
@@ -0,0 +1,73 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 3;
+    int y = 3;
+    y =y+ 2;
+
+    Console.WriteLine(y);
+    y += 2;
+    Console.WriteLine(y);
+    y /= 1;
+    Console.WriteLine(x);
+    x *= 2;
+//Mantıksal operatörler
+
+    bool isSuccess = true;
+    bool isCompleted = false;
+
+    if (isSuccess && isCompleted)
+    {
+        Console.WriteLine("Perfect!");
+    }
+
+    if (isSuccess || isCompleted)
+    {
+        Console.WriteLine("Great!");
+    }
+
+    if (isSuccess && !isCompleted)
+    {
+        Console.WriteLine("Fine!");
+    }
+//İlişkisel Operatörler
+
+    int a =3;
+    int b = 4;
+    bool sonuc = a<b;
+    
+    Console.WriteLine(sonuc);
+    sonuc= a>b;
+    Console.WriteLine(sonuc);
+    sonuc= a>=b;
+    Console.WriteLine(sonuc);
+    sonuc= a<=b;
+    Console.WriteLine(sonuc);
+    sonuc= a==b;
+    Console.WriteLine(sonuc);
+    sonuc= a!=b;
+    Console.WriteLine(sonuc);
+    
+    
+    //Aritmetik Operatörler
+
+    
+    int sayi1 =10;
+    int sayi2 =5;
+    
+
+    int sonuc1= sayi1/sayi2;
+    Console.WriteLine(sonuc1);
+    sonuc1=sayi1*sayi2;
+    Console.WriteLine(sonuc1);
+    sonuc1=sayi1+sayi2;
+    Console.WriteLine(sonuc1);
+    sonuc1=sayi1++;
+    Console.WriteLine(sonuc1);
+// % --> mod alır
+    int sonuc2 = 20%3;
+    Console.WriteLine(sonuc2);
+
+    }
+}
