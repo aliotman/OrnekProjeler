6293ab35b9b8c4328ac0ddf0b6de331281e7b137
Ali Otman <aliotmangitar@hotmail.com>
2022-08-14 00:56:40 +0300

    Hata Yonetimi Ornekler Eklendi

---
+++ HataYonetimi/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+        Console.WriteLine("Bir Sayi Giriniz");
+        int sayi=Convert.ToInt32(Console.Readline());
+        Console.WriteLine("Girmiş olduğunuz sayı şudur: "+sayi); 
+        }
+        catch(Exception ex)
+        {
+        Console.WriteLine("Hata: "+ex.ToString());
+        }
+        // finally
+        // {
+        //     Console.WriteLine("İşlem tamamlandı.");
+        // }
+
+        try
+        {
+           // int a=int.Parse(null);
+           // int a=int.Parse("test");
+           int a=int.Parse("-20000000000");
+        }
+        catch(ArgumanNullException ex)
+        {
+            Console.WriteLine("Boş değer girdiniz!");
+            Console.WriteLine(ex);
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Veri Tipi Uygun Değil!");
+            Console.WriteLine(ex);
+        }
+        catch(OverflowException ex)
+        {
+            Console.WriteLine("Çok küçük veya çok büyük bir değer girdiniz!");
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            System.Console.WriteLine("İşlem başarıyla tamamlandı.");
+        }
+       
+    }
+}
