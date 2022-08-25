6a42d5bfd3ccf00d1847deb6c7205546a53e0899
Ali Otman <aliotmangitar@hotmail.com>
2022-08-26 01:56:52 +0300

    Metot Ornek Eklendi

---
+++ Metotlar/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ///Erişim_belirleyici geridönüş_tipi metot_adı(parametreler_listesi//arguman)
+        //{
+            //komutlar;
+            //return;
+        //}
+        int a=2;
+        int b=3;
+        Console.WriteLine(a+b);
+
+        int sonuc=Topla(a,b);
+        Console.WriteLine(sonuc);
+
+        Metotlar ornek= new Metotlar();
+        ornek.EkranaYazdir(sonuc.ToString());
+    }
+    static int Topla(int deger1,int deger2)
+    {
+        return(deger1+deger2);
+    }
+}
+class Metotlar
+{
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public int ArttirveTopla(ref int deger1,ref int deger2)
+    {
+        deger1+=1;
+        deger2+=1;
+        return deger1+deger2;
+    }
+}
