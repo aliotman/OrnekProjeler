501979163c65dce1d0c446a4314a26438878da08
Ali Otman <aliotmangitar@hotmail.com>
2022-09-22 03:41:34 +0300

    Enum Ornek Eklendi

---
+++ Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Gunler.Pazar);
+        Console.WriteLine((int)Gunler.Cumartesi);
+        int sicaklik=32;
+
+        if(sicaklik<=(int)HavaDurumu.Normal)
+        {
+            System.Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+        }
+        else if(sicaklik>=(int)HavaDurumu.CokSicak)
+        {
+            System.Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+        }
+        else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSicak)
+        {
+            System.Console.WriteLine("Hadi dışarıya çıkalım.");
+        }
+    }
+}
+enum Gunler
+{
+    Pazartesi,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal=20,
+    Sicak=25,
+    CokSicak=30
+}
